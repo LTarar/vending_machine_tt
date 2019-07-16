43b50d8cba0e9f52187d926b5128155b2cf989e4
Wardah <wlt_93@hotmail.com>
2019-07-16 17:13:33 +0100

    Add price attribute to product class

--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -1,9 +1,13 @@
 require 'product'
 
 describe Product do
-  subject(:product) {Product.new('Coke')}
+  subject(:product) {Product.new('Coke',70)}
 
   it 'checks for a product name' do
     expect(product.name).to eq 'Coke'
+  end
+
+  it 'checks for a product price' do
+    expect(product.price).to eq 70
   end
 end
