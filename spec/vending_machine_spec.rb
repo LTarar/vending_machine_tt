a04b108c22960f939121f1f06a14ffb4aea0253f
Wardah <wlt_93@hotmail.com>
2019-07-16 17:23:56 +0100

    Vending machine to have an empty array of products

--- spec/vending_machine_spec.rb
+++ spec/vending_machine_spec.rb
@@ -1,4 +1,9 @@
 require 'vending_machine'
 
 describe VendingMachine do
+  subject(:vending_machine) {VendingMachine.new}
+
+  it 'checks if vending machine has an empty array of products' do
+    expect(vending_machine.products).to eq []
+  end
 end
