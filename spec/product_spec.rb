34c6d29f3f11bfcde85291478bdff6f068ce19b7
Wardah <wlt_93@hotmail.com>
2019-07-16 17:15:29 +0100

    Add quantity attribute to product class

--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -1,7 +1,7 @@
 require 'product'
 
 describe Product do
-  subject(:product) {Product.new('Coke',70)}
+  subject(:product) {Product.new('Coke',70,10)}
 
   it 'checks for a product name' do
     expect(product.name).to eq 'Coke'
@@ -9,5 +9,9 @@ describe Product do
 
   it 'checks for a product price' do
     expect(product.price).to eq 70
+  end
+
+  it 'checks for a product quantity' do
+    expect(product.quantity).to eq 10
   end
 end
