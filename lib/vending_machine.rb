73af1738d2748e925e9454b8791f2b21f898dc26
Wardah <wlt_93@hotmail.com>
2019-07-17 09:59:20 +0100

    Refactor - Extract default list to a method

--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -4,7 +4,14 @@ class VendingMachine
   attr_reader :products
 
   def initialize
-    @products = [
+    @products = default_products
+  end
+
+
+  private
+  
+  def default_products
+    [
       Product.new('Coke',70,10),
       Product.new('Pepsi',60,10), 
       Product.new('7up',55,10),
